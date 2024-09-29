a526fa8e66abce6e874b7f58bee83e5aadf006fd
xxx-irfan <sehzadikhan632@gmail.com>
2024-09-29 23:14:16 +0530

    Update 0_Bestie.js

--- modules/commands/0_Bestie.js
+++ modules/commands/0_Bestie.js
@@ -66,7 +66,7 @@ module.exports.run = async function ({ event, api, args }) {
   const fs = global.nodemodule["fs-extra"];
   const { threadID, messageID, senderID } = event;
   const mention = Object.keys(event.mentions);
-  if (!mention[0]) return api.sendMessage("Kisi 1 ko mantion to kr tootiye 😅", threadID, messageID);
+  if (!mention[0]) return api.sendMessage("𝐀𝐋𝐄 𝐓𝐔𝐓𝐈𝐘𝐀 𝐊𝐈𝐒𝐈 𝐄𝐊 𝐊𝐎 𝐌𝐄𝐍𝐓𝐈𝐎𝐍 𝐊𝐀𝐑😅", threadID, messageID);
   else {
       const one = senderID, two = mention[0];
       return makeImage({ one, two }).then(path => api.sendMessage({ body: "✧•❁𝐅𝐫𝐢𝐞𝐧𝐝𝐬𝐡𝐢𝐩❁•✧\n\n╔═══❖••° °••❖═══╗\n\n   𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐏𝐚𝐢𝐫𝐢𝐧𝐠\n\n╚═══❖••° °••❖═══╝\n\n   ✶⊶⊷⊷❍⊶⊷⊷✶\n\n       👑𝐘𝐄 𝐋𝐄 𝐌𝐈𝐋 𝐆𝐀𝐈 ❤\n\n𝐓𝐄𝐑𝐈 𝐁𝐄𝐒𝐓𝐈𝐄 🩷\n\n   ✶⊶⊷⊷❍⊶⊷⊷✶", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
