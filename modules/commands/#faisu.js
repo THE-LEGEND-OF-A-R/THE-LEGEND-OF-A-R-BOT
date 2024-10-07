36c3652ff2575a16d0b1f5985dce1c27977cc915
xxx-irfan <sehzadikhan632@gmail.com>
2024-10-07 09:52:48 +0530

    Create #faisu.js

---
+++ modules/commands/#faisu.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "taime",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "𝐏𝐑𝐈𝐘𝐀𝐍𝐒𝐇𝐈 𝐊𝐀𝐔𝐑", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("Faisu") ||
+     react.includes("faisu") || react.includes("faisu khan") || react.includes("@Faisu khan") ||
+react.includes("@faisu khan") ||
+react.includes("FAISU KHAN") ||     
+react.includes("FAISU")) {
+    var msg = {
+        body: `${name}  𝐀𝐁 𝐘𝐀𝐀𝐋 𝐀𝐁𝐇𝐈 𝐌𝐀𝐈 𝐏𝐀𝐑𝐄𝐒𝐇𝐀𝐍 𝐇𝐔𝐍 𝐋𝐀𝐃𝐊𝐈 𝐊𝐄 𝐂𝐇𝐀𝐊𝐊𝐑 𝐌𝐄 𝐏𝐀𝐈𝐑 𝐏𝐄 𝐊𝐔𝐋𝐀𝐃𝐈 𝐌𝐀𝐑 𝐋i
+        जब तक सूरज चांद रहेगा 
+तब तक, तेरी बेज्जती करना 
+मेरा काम रहेगा।
+        💐✿`,attachment: fs.createReadStream(__dirname + `/noprefix/Faisu.gif`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🙃", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+
+  }
