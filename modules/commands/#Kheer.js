97a40631e90e374d930ee30be9b6e2771971b441
xxx-irfan <sehzadikhan632@gmail.com>
2024-10-25 08:42:51 +0530

    Update #Kheer.js

--- modules/commands/#Kheer.js
+++ modules/commands/#Kheer.js
@@ -21,7 +21,7 @@ react.includes("khir ")) {
     var msg = {
         body: `${name} 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐀𝐏𝐊𝐀 𝐊𝐇𝐄𝐄𝐑 𝐀𝐆𝐀𝐘𝐀💐✿
  *╔═══❖•⊰ ☞𓅂 𝐌𝐀𝐃𝐄 ♡ 𝐁𝐘 ♡ 𝐀𝐑𝐘𝐍𝐀𓅂☜ ⊱•❖═══╗*`,
-attachment: fs.createReadStream(__dirname + `/noprefix/dosa.jpeg`)
+attachment: fs.createReadStream(__dirname + `/noprefix/Kheer.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤤", event.messageID, (err) => {}, true)
